c5172d8094505ac2f672a0d7a8775b8f69a5e682
Georgi Dzhankushev <georgi.dzhankushev@gmail.com>
2021-08-11 09:15:28 +0300

    done with connecting to DB

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 A simple REST API with Node.js and Express
 
 To-do:
-- Connect to MySQL or Mongo DB (in progress)
 - Input validation with express-validator / JOI?
 - add options other than JSON for response format based on content-type header
 - Add JWT or other authentication
+- Add branch that uses Mongo DB instead?
 - Migrate to TypeScript?
--- middleware.js
+++ middleware.js
@@ -4,16 +4,20 @@ module.exports = {
 		console.log(`${timestamp}\t${req.ip}\t${req.method} ${req.originalUrl}`);
 		next();
 	},
-	notFound: (req, res) => {
-		res.status(404).send();
-	},
-	methodUnsupported: (req, res) => {
-		res.status(405).send();
-	},
+
+	notFound: (req, res) => res.status(404).send(),
+
+	methodUnsupported: (req, res) => res.status(405).send(),
+
 	handleError: (err, req, res, next) => {
-		console.log(err);
-		res.status(500).send('There was a problem on our end');
+		if ( err.message.includes('Unexpected token') && err.message.includes('in JSON') )
+			res.status(400).send();
+		else {
+			console.log(err);
+			res.status(500).send('There was a problem on our end');
+		}
 	},
+
 	getRoot: (req, res) => {
 		sql.query('SHOW TABLES', (err, results) => {
 			// key contains the name of the db; better to get it this way than hard-code it
@@ -23,6 +27,7 @@ module.exports = {
 			res.send( {tables: tables} );
 		});
 	},
+
 	getCustomers: (req, res) => {
 		sql.query('SELECT name, age FROM customers', (err, results) => {
 			let customers = [];
@@ -30,9 +35,21 @@ module.exports = {
 			res.send( {customers: customers} );
 		});
 	},
+
 	postCustomers: (req, res) => {
-		res.send(`POST customers received: ${ JSON.stringify(req.body) }`);
+		if (Object.keys(req.body).length < 2)
+			return res.status(400).send();
+
+		const name = req.body.name;
+		const age = parseInt(req.body.age);
+		if ( !name.length || isNaN(age) )
+			return res.status(400).send();
+
+		sql.query("INSERT INTO customers (name, age) VALUES (?, ?)", [name, age], (err, results) => {
+			res.status(201).send( {id: results.insertId} );
+		});
 	},
+
 	getCustomer: (req, res) => {
 		const id = parseInt(req.params.id);
 		if (isNaN(id))
@@ -43,10 +60,32 @@ module.exports = {
 			else res.send( {name: results[0].name, age: results[0].age} );
 		});
 	},
+
 	putCustomer: (req, res) => {
-		res.send(`Hello from PUT customer ${req.params.id}`);
+		const id = parseInt(req.params.id);
+		if (isNaN(id) || Object.keys(req.body).length < 2)
+			return res.status(400).send();
+
+		const name = req.body.name;
+		const age = parseInt(req.body.age);
+		if ( !name.length || isNaN(age) )
+			return res.status(400).send();
+
+		sql.query("UPDATE customers SET name = ?, age = ? WHERE id = ?", [name, age, id], (err, results) => {
+			if (results.changedRows)
+				res.send();
+			else if (results.affectedRows)
+				res.send('nothing changed');
+			else res.status(404).send();
+		});
 	},
+
 	deleteCustomer: (req, res) => {
-		res.send(`Hello from DELETE customer ${req.params.id}`);
+		const id = parseInt(req.params.id);
+		if (isNaN(id))
+			res.status(400).send();
+		else sql.query(`DELETE FROM customers WHERE id = ${id}`, (err, results) => {
+			results.affectedRows ? res.send() : res.status(404).send();
+		});
 	}
 }
